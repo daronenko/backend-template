76a4b2055db1f246e33c93591a326595c577135b
Daniil Mironenko <dan.mironenko@gmail.com>
2025-01-04 01:37:53 +0300

    implement logger interface with zap

---
+++ internal/logger/zap/zap.go
@@ -0,0 +1,289 @@
+package zap
+
+import (
+	"os"
+	"time"
+
+	"github.com/daronenko/backend-template/internal/constants"
+	"github.com/daronenko/backend-template/internal/logger"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Application logger
+type zapLogger struct {
+	level       string
+	devMode     bool
+	encoding    string
+	sugarLogger *zap.SugaredLogger
+	logger      *zap.Logger
+}
+
+// NewZapLogger App Logger constructor
+func New(cfg *logger.Config) *zapLogger {
+	return &zapLogger{level: cfg.LogLevel, devMode: cfg.DevMode, encoding: cfg.Encoder}
+}
+
+// For mapping config logger to email_service logger levels
+var loggerLevelMap = map[string]zapcore.Level{
+	"debug":  zapcore.DebugLevel,
+	"info":   zapcore.InfoLevel,
+	"warn":   zapcore.WarnLevel,
+	"error":  zapcore.ErrorLevel,
+	"dpanic": zapcore.DPanicLevel,
+	"panic":  zapcore.PanicLevel,
+	"fatal":  zapcore.FatalLevel,
+}
+
+func (l *zapLogger) getLoggerLevel() zapcore.Level {
+	level, exist := loggerLevelMap[l.level]
+	if !exist {
+		return zapcore.DebugLevel
+	}
+
+	return level
+}
+
+// InitLogger Init logger
+func (l *zapLogger) InitLogger() {
+	logLevel := l.getLoggerLevel()
+
+	logWriter := zapcore.AddSync(os.Stdout)
+
+	var encoderCfg zapcore.EncoderConfig
+	if l.devMode {
+		encoderCfg = zap.NewDevelopmentEncoderConfig()
+	} else {
+		encoderCfg = zap.NewProductionEncoderConfig()
+	}
+
+	var encoder zapcore.Encoder
+	encoderCfg.NameKey = "[SERVICE]"
+	encoderCfg.TimeKey = "[TIME]"
+	encoderCfg.LevelKey = "[LEVEL]"
+	encoderCfg.CallerKey = "[LINE]"
+	encoderCfg.MessageKey = "[MESSAGE]"
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	encoderCfg.EncodeLevel = zapcore.CapitalLevelEncoder
+	encoderCfg.EncodeCaller = zapcore.ShortCallerEncoder
+	encoderCfg.EncodeDuration = zapcore.StringDurationEncoder
+
+	if l.encoding == "console" {
+		encoderCfg.EncodeLevel = zapcore.CapitalColorLevelEncoder
+		encoderCfg.EncodeCaller = zapcore.FullCallerEncoder
+		encoderCfg.ConsoleSeparator = " | "
+		encoder = zapcore.NewConsoleEncoder(encoderCfg)
+	} else {
+		encoderCfg.FunctionKey = "[CALLER]"
+		encoderCfg.EncodeName = zapcore.FullNameEncoder
+		encoder = zapcore.NewJSONEncoder(encoderCfg)
+	}
+
+	core := zapcore.NewCore(encoder, logWriter, zap.NewAtomicLevelAt(logLevel))
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+
+	l.logger = logger
+	l.sugarLogger = logger.Sugar()
+}
+
+// Logger methods
+
+// WithName add logger microservice name
+func (l *zapLogger) WithName(name string) {
+	l.logger = l.logger.Named(name)
+	l.sugarLogger = l.sugarLogger.Named(name)
+}
+
+// Debug uses fmt.Sprint to construct and log a message.
+func (l *zapLogger) Debug(args ...interface{}) {
+	l.sugarLogger.Debug(args...)
+}
+
+// Debugf uses fmt.Sprintf to log a templated message
+func (l *zapLogger) Debugf(template string, args ...interface{}) {
+	l.sugarLogger.Debugf(template, args...)
+}
+
+// Debugw logs a message with some additional context.
+func (l *zapLogger) Debugw(msg string, fields logger.Fields) {
+	zapFields := mapToZapFields(fields)
+	l.logger.Debug(msg, zapFields...)
+}
+
+// Info uses fmt.Sprint to construct and log a message
+func (l *zapLogger) Info(args ...interface{}) {
+	l.sugarLogger.Info(args...)
+}
+
+// Infof uses fmt.Sprintf to log a templated message.
+func (l *zapLogger) Infof(template string, args ...interface{}) {
+	l.sugarLogger.Infof(template, args...)
+}
+
+// Infow logs a message with some additional context.
+func (l *zapLogger) Infow(msg string, fields logger.Fields) {
+	zapFields := mapToZapFields(fields)
+	l.logger.Info(msg, zapFields...)
+}
+
+// Printf uses fmt.Sprintf to log a templated message
+func (l *zapLogger) Printf(template string, args ...interface{}) {
+	l.sugarLogger.Infof(template, args...)
+}
+
+// Warn uses fmt.Sprint to construct and log a message.
+func (l *zapLogger) Warn(args ...interface{}) {
+	l.sugarLogger.Warn(args...)
+}
+
+// WarnMsg log error message with warn level.
+func (l *zapLogger) WarnMsg(msg string, err error) {
+	l.logger.Warn(msg, zap.String("error", err.Error()))
+}
+
+// Warnf uses fmt.Sprintf to log a templated message.
+func (l *zapLogger) Warnf(template string, args ...interface{}) {
+	l.sugarLogger.Warnf(template, args...)
+}
+
+// Error uses fmt.Sprint to construct and log a message.
+func (l *zapLogger) Error(args ...interface{}) {
+	l.sugarLogger.Error(args...)
+}
+
+// Errorf uses fmt.Sprintf to log a templated message.
+func (l *zapLogger) Errorf(template string, args ...interface{}) {
+	l.sugarLogger.Errorf(template, args...)
+}
+
+// Err uses error to log a message.
+func (l *zapLogger) Err(msg string, err error) {
+	l.logger.Error(msg, zap.Error(err))
+}
+
+// Errorw logs a message with some additional context.
+func (l *zapLogger) Errorw(msg string, fields logger.Fields) {
+	zapFields := mapToZapFields(fields)
+	l.logger.Error(msg, zapFields...)
+}
+
+// DPanic uses fmt.Sprint to construct and log a message. In development, the logger then panics. (See DPanicLevel for details.)
+func (l *zapLogger) DPanic(args ...interface{}) {
+	l.sugarLogger.DPanic(args...)
+}
+
+// DPanicf uses fmt.Sprintf to log a templated message. In development, the logger then panics. (See DPanicLevel for details.)
+func (l *zapLogger) DPanicf(template string, args ...interface{}) {
+	l.sugarLogger.DPanicf(template, args...)
+}
+
+// Panic uses fmt.Sprint to construct and log a message, then panics.
+func (l *zapLogger) Panic(args ...interface{}) {
+	l.sugarLogger.Panic(args...)
+}
+
+// Panicf uses fmt.Sprintf to log a templated message, then panics
+func (l *zapLogger) Panicf(template string, args ...interface{}) {
+	l.sugarLogger.Panicf(template, args...)
+}
+
+// Fatal uses fmt.Sprint to construct and log a message, then calls os.Exit.
+func (l *zapLogger) Fatal(args ...interface{}) {
+	l.sugarLogger.Fatal(args...)
+}
+
+// Fatalf uses fmt.Sprintf to log a templated message, then calls os.Exit.
+func (l *zapLogger) Fatalf(template string, args ...interface{}) {
+	l.sugarLogger.Fatalf(template, args...)
+}
+
+// Sync flushes any buffered log entries
+func (l *zapLogger) Sync() error {
+	go l.logger.Sync() // nolint: errcheck
+	return l.sugarLogger.Sync()
+}
+
+func (l *zapLogger) HttpMiddlewareAccessLogger(
+	method,
+	uri string,
+	status int,
+	size int64,
+	time time.Duration,
+) {
+	l.logger.Info(
+		constants.HTTP,
+		zap.String(constants.METHOD, method),
+		zap.String(constants.URI, uri),
+		zap.Int(constants.STATUS, status),
+		zap.Int64(constants.SIZE, size),
+		zap.Duration(constants.TIME, time),
+	)
+}
+
+func (l *zapLogger) GrpcMiddlewareAccessLogger(
+	method string,
+	time time.Duration,
+	metaData map[string][]string,
+	err error,
+) {
+	l.logger.Info(
+		constants.GRPC,
+		zap.String(constants.METHOD, method),
+		zap.Duration(constants.TIME, time),
+		zap.Any(constants.METADATA, metaData),
+		zap.Error(err),
+	)
+}
+
+func (l *zapLogger) GrpcClientInterceptorLogger(
+	method string,
+	req interface{},
+	reply interface{},
+	time time.Duration,
+	metaData map[string][]string,
+	err error,
+) {
+	l.logger.Info(
+		constants.GRPC,
+		zap.String(constants.METHOD, method),
+		zap.Any(constants.REQUEST, req),
+		zap.Any(constants.REPLY, reply),
+		zap.Duration(constants.TIME, time),
+		zap.Any(constants.METADATA, metaData),
+		zap.Error(err),
+	)
+}
+
+func mapToZapFields(data map[string]interface{}) []zap.Field {
+	fields := make([]zap.Field, 0, len(data))
+
+	for key, value := range data {
+		field := zap.Field{
+			Key:       key,
+			Type:      getFieldType(value),
+			Interface: value,
+		}
+		fields = append(fields, field)
+	}
+
+	return fields
+}
+
+func getFieldType(value interface{}) zapcore.FieldType {
+	switch value.(type) {
+	case string:
+		return zapcore.StringType
+	case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+		return zapcore.Int64Type
+	case bool:
+		return zapcore.BoolType
+	case float32, float64:
+		return zapcore.Float64Type
+	case error:
+		return zapcore.ErrorType
+	default:
+		// uses reflection to serialize arbitrary objects, so it can be slow and allocation-heavy.
+		return zapcore.ReflectType
+	}
+}
