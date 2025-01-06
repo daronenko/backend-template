f6263073fbf2a9b71aa3b9ba2ee7d9777a9e89e1
Daniil Mironenko <dan.mironenko@gmail.com>
2025-01-06 17:17:33 +0300

    add more constants

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,21 +1,14 @@
 package constants
 
 const (
-	ConfigPath = "CONFIG_PATH"
+	ConfigPathEnv = "CONFIG_PATH"
 
 	Env  = "env"
 	Yaml = "yaml"
 	Json = "json"
 
-	GRPC     = "GRPC"
-	SIZE     = "SIZE"
-	URI      = "URI"
-	STATUS   = "STATUS"
-	HTTP     = "HTTP"
-	ERROR    = "ERROR"
-	METHOD   = "METHOD"
-	METADATA = "METADATA"
-	REQUEST  = "REQUEST"
-	REPLY    = "REPLY"
-	TIME     = "TIME"
+	Error = "error"
+
+	Version  = "version"
+	Revision = "revision"
 )
