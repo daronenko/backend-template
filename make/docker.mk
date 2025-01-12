aba6edcd0cd7ad146a3a0ff8a389922aabe537b3
Daniil Mironenko <dan.mironenko@gmail.com>
2025-01-12 14:24:09 +0300

    optimize docker make targets

--- make/docker.mk
+++ make/docker.mk
@@ -1,31 +1,28 @@
 .PHONY: docker/build docker/build/hot docker/run docker/down docker/clean docker/logs docker/logs/live
 
 DOCKER_COMPOSE_PATH ?= deploy/dev/docker-compose.yaml
-DOCKER_COMPOSE 		?= docker compose -f ${DOCKER_COMPOSE_PATH}
-
-# compose (default: all)
-c ?=
+DOCKER_COMPOSE 		?= docker compose -f ${DOCKER_COMPOSE_PATH} --profile ${PROFILE}
 
 db: docker/build
 docker/build:
-	VERSION=${VERSION} REVISION=${REVISION} ${DOCKER_COMPOSE} --profile ${PROFILE} build
+	@VERSION=${VERSION} REVISION=${REVISION} ${DOCKER_COMPOSE} build
 
 dr: docker/run
 docker/run:
-	@${DOCKER_COMPOSE} --profile ${PROFILE} up -d
+	@${DOCKER_COMPOSE} up -d
 
 dd: docker/down
 docker/down:
-	@${DOCKER_COMPOSE} --profile ${PROFILE} down
+	@${DOCKER_COMPOSE} down
 
 dc: docker/clean
 docker/clean:
-	@${DOCKER_COMPOSE} --profile ${PROFILE} down -v --rmi all
+	@${DOCKER_COMPOSE} down -v --rmi all
 
 dl: docker/logs
 docker/logs:
-	@${DOCKER_COMPOSE} --profile ${PROFILE} logs ${c}
+	${DOCKER_COMPOSE} logs ${c} ${compose}
 
 dll: docker/logs/live
 docker/logs/live:
-	@${DOCKER_COMPOSE} --profile ${PROFILE} logs -f ${c}
+	@${DOCKER_COMPOSE} logs -f ${c} ${compose}
