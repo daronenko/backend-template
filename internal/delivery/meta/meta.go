97f2698dbc4f6e370678b2bc7a708e4a07de92a5
Daniil Mironenko <dan.mironenko@gmail.com>
2025-01-12 23:14:52 +0300

    update build script inside hot reload setup

--- internal/delivery/meta/meta.go
+++ internal/delivery/meta/meta.go
@@ -1,6 +1,7 @@
 package meta
 
 import (
+	"github.com/daronenko/backend-template/internal/config"
 	"github.com/daronenko/backend-template/internal/server/svr"
 	"github.com/gofiber/fiber/v2"
 	"go.uber.org/fx"
@@ -12,10 +13,18 @@ type Meta struct {
 
 func RegisterMeta(meta *svr.Meta, c Meta) {
 	meta.Get("/health", c.Health)
+	meta.Get("/build", c.Build)
 }
 
 func (c *Meta) Health(ctx *fiber.Ctx) error {
 	return ctx.JSON(fiber.Map{
 		"status": "ok",
+	})
+}
+
+func (c *Meta) Build(ctx *fiber.Ctx) error {
+	return ctx.JSON(fiber.Map{
+		"version":  config.Version,
+		"revision": config.Revision,
 	})
 }
