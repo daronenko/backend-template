87d1adca6fdcdc9ca2bc94ea2290fbb00fad5f07
Daniil Mironenko <dan.mironenko@gmail.com>
2025-01-12 14:13:16 +0300

    setup docker compose profiles

--- make/docker.mk
+++ make/docker.mk
@@ -3,32 +3,29 @@
 DOCKER_COMPOSE_PATH ?= deploy/dev/docker-compose.yaml
 DOCKER_COMPOSE 		?= docker compose -f ${DOCKER_COMPOSE_PATH}
 
+# compose (default: all)
+c ?=
+
 db: docker/build
 docker/build:
-	@VERSION=${VERSION} REVISION=${REVISION} ${DOCKER_COMPOSE} build
+	VERSION=${VERSION} REVISION=${REVISION} ${DOCKER_COMPOSE} --profile ${PROFILE} build
 
 dr: docker/run
 docker/run:
-	@${DOCKER_COMPOSE} up -d
+	@${DOCKER_COMPOSE} --profile ${PROFILE} up -d
 
 dd: docker/down
 docker/down:
-	@${DOCKER_COMPOSE} down
+	@${DOCKER_COMPOSE} --profile ${PROFILE} down
 
 dc: docker/clean
 docker/clean:
-	@${DOCKER_COMPOSE} down -v --rmi all
-
-c       ?=
-compose ?=
+	@${DOCKER_COMPOSE} --profile ${PROFILE} down -v --rmi all
 
 dl: docker/logs
 docker/logs:
-	@${DOCKER_COMPOSE} logs ${compose} ${c}
-
-c       ?=
-compose ?=
+	@${DOCKER_COMPOSE} --profile ${PROFILE} logs ${c}
 
 dll: docker/logs/live
 docker/logs/live:
-	@${DOCKER_COMPOSE} logs -f ${compose} ${c}
+	@${DOCKER_COMPOSE} --profile ${PROFILE} logs -f ${c}
